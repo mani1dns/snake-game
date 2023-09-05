519ed48da7350b517c24ba9afcc97eb80ad761ae
Mani A <mani.msd1357@gmail.com>
2023-09-05 09:07:55 +0530

    snake body is created by for loop

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Python Snake Game")
 
-segment_1 = Turtle("square")
-segment_1.color("red")
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("red")
+    new_segment.goto(position)
 
 segment_2 = Turtle("square")
 segment_2.color("red")
