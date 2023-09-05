5f112f99bf6bdaff975cded5fa67de74b363ce66
Mani A <mani.msd1357@gmail.com>
2023-09-05 08:43:24 +0530

    body of snake is created

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Python Snake Game")
 
+segment_1 = Turtle("square")
+segment_1.color("red")
 
+segment_2 = Turtle("square")
+segment_2.color("red")
+segment_2.goto(-20, 0)
 
-
-
-
-
+segment_3 = Turtle("square")
+segment_3.color("red")
+segment_3.goto(-40, 0)
 
 
 
